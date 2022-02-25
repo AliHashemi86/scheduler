a85f0b9bbabe7f2a83d94c55530c6858c01e0e30
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-02-25 09:52:20 -0500

    display  Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,12 +3,14 @@ import "./styles.scss";
 import Header from "./Header.js";
 import Empty from "./Empty.js";
 import Show from "./Show.js";
-import Status from "./Status.js";
-import Confirm from "./Confirm.js";
-import Form from "./Form.js";
+
 
 export default function Appointment(props) {
   return (
-    <article className="appointment">{props.time}</article>
+  
+    <article className="appointment">
+      <Header time={props.time}/>
+      { props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/> }
+    </article>
   )
 }
