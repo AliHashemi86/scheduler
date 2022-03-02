70659a5bc0d4bb56cab44a05f1e5f24f802fd3c4
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-03-02 18:40:22 -0500

    add cypress

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -15,7 +15,7 @@ export default function DayListItem(props) {
       : `${spots} spots remaining`;
   };
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass} data-testid='day'>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
